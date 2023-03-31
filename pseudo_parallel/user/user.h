a49adae69affd7a90e24e61ee67e5ddb4c2a5c21
Ganesh Rai <53878675+petermultiverse1998@users.noreply.github.com>
2023-03-31 09:08:19 +0545

    v1.0.0

---
+++ pseudo_parallel/user/user.h
@@ -0,0 +1,19 @@
+/*
+ * user.h
+ *
+ *  Created on: Feb 21, 2023
+ *      Author: peter
+ */
+
+#ifndef USER_H_
+#define USER_H_
+
+void systickHandler();
+void SVCHandler();
+void pendSVHandler();
+
+void init();
+void loop();
+
+
+#endif /* USER_H_ */
